27e61229da12009bc6529850127dbcd9d80913bf
Dallas Sybrowsky <dallas.sybrowsky@gmail.com>
2022-12-28 15:52:08 -0800

    Updated files in models folder, server.js & formatted .env file

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -5,12 +5,24 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: ''
+})
 
 // Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: ''
+})
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongToMany(Tag, {
+  foreignKey: ''
+})
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongToMany(Product, {
+  foreignKey: 'ProductTag'
+})
 
 module.exports = {
   Product,
